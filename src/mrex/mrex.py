e6b721c10d05b3bcaee7f46b52ebbbfc6079343a
Gabay <roigby@gmail.com>
2023-03-05 23:24:17 +0200

    mrex: add & test functionality.

--- src/mrex/mrex.py
+++ src/mrex/mrex.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import re
-from typing import Iterator, Optional
+from typing import Optional
 
 
 class MagicRegex:
@@ -10,7 +10,11 @@ class MagicRegex:
         self._regex = None
 
     @property
-    def regex(self):
+    def text(self) -> str:
+        return self._text
+
+    @property
+    def regex(self) -> re.Pattern:
         if self._regex is None:
             self._regex = re.compile(self._text, re.DOTALL)
         return self._regex
@@ -20,25 +24,22 @@ class MagicRegex:
     def find(self, text: str) -> Optional[re.Match[str]]:
         return self.regex.search(text)
 
-    def find_all(self, text: str) -> Iterator[re.Match[str]]:
-        return self.regex.finditer(text)
+    def find_all(self, text: str) -> list[re.Match[str]]:
+        return self.regex.findall(text)
 
     # Repeat
 
     def repeat(self, n: int) -> MagicRegex:
-        return MagicRegex(f"(?:{self._text}){{{n}}}")
-
-    def repeat_min_max(self, n: int, m: int) -> MagicRegex:
-        return MagicRegex(f"(?:{self._text}){{{n},{m}}}")
+        return MagicRegex(f"{self._grouped_text}{{{n}}}")
 
     def repeat_zero_or_more(self) -> MagicRegex:
-        return MagicRegex(f"(?:{self._text})*")
+        return MagicRegex(f"{self._grouped_text}*")
 
     def repeat_one_or_more(self) -> MagicRegex:
-        return MagicRegex(f"(?:{self._text})+")
+        return MagicRegex(f"{self._grouped_text}+")
 
     def optional(self) -> MagicRegex:
-        return MagicRegex(f"(?:{self._text})?")
+        return MagicRegex(f"{self._grouped_text}?")
 
     # Group
 
@@ -50,12 +51,16 @@ class MagicRegex:
     # Concat
 
     def or_(self, other: str | MagicRegex) -> MagicRegex:
-        other_rex = other if isinstance(other, MagicRegex) else MagicRegex(other)
-        return MagicRegex(f"(?:{self._text})|(?:{other_rex._text})")
+        return any_of([self, other])
 
     def and_(self, other: str | MagicRegex) -> MagicRegex:
-        other_rex = other if isinstance(other, MagicRegex) else MagicRegex(other)
-        return MagicRegex(f"(?:{self._text})(?:{other_rex._text})")
+        return concat([self, other])
+
+    # Internal functionality
+
+    @property
+    def _grouped_text(self) -> str:
+        return f"(?:{self.text})"
 
 
 def exactly(text: str) -> MagicRegex:
@@ -73,10 +78,19 @@ def char_not_in(chars: str) -> MagicRegex:
     return MagicRegex(f"[^{escaped_chars}]")
 
 
-def any_of(options: list[str | MagicRegex]) -> MagicRegex:
-    options_objects = [
+def any_of(items: list[str | MagicRegex]) -> MagicRegex:
+    mrex_objects = [
         option if isinstance(option, MagicRegex) else exactly(option)
-        for option in options
+        for option in items
     ]
-    any_of_str = "|".join([f"(?:{match._text})" for match in options_objects])
+    any_of_str = "|".join([obj._grouped_text for obj in mrex_objects])
     return MagicRegex(any_of_str)
+
+
+def concat(items: list[str | MagicRegex]) -> MagicRegex:
+    mrex_objects = [
+        option if isinstance(option, MagicRegex) else exactly(option)
+        for option in items
+    ]
+    concatenated_str = "".join([obj._grouped_text for obj in mrex_objects])
+    return MagicRegex(concatenated_str)
--- tests/mrex/test_mrex.py
+++ tests/mrex/test_mrex.py
@@ -50,7 +50,107 @@ def test_any_of_should_return_object_on_match_success():
     assert mrex_obj.find(words[2]) is not None
 
 
-def test_any_of_should_return_none_on_match_failure():
+def test_any_of_should_return_none_on_match_fail():
     words = ["hello", "world", "!!!"]
     mrex_obj = mrex.any_of(words)
     assert mrex_obj.find("hola") is None
+
+
+# concat()
+
+
+def test_concat_should_return_object_on_match_success():
+    words = ["hello", "world", "!!!"]
+    assert mrex.concat(words).find("".join(words)) is not None
+
+
+def test_concat_should_return_none_on_match_fail():
+    words = ["hello", "world", "!!!"]
+    assert mrex.concat(words).find(" ".join(words)) is None
+
+
+# repeat()
+
+
+def test_repeat_should_match_on_success():
+    assert mrex.DIGIT.repeat(3).find("aA12345").group() == "123"
+
+
+def test_repeat_should_return_none_on_fail():
+    assert mrex.DIGIT.repeat(3).find("aA12") is None
+
+
+# repeat_zero_or_more()
+
+
+def test_repeat_zero_or_more_should_match_on_success():
+    text = "!@#$%^&*()_+"
+    mrex_obj = mrex.exactly(text).repeat_zero_or_more()
+    assert mrex_obj.find(text).group() == text
+    assert mrex_obj.find(text * 2).group() == text * 2
+    assert mrex_obj.find("!!!").group() == ""
+
+
+# repeat_one_or_more()
+
+
+def test_repeat_one_or_more_should_match_on_success():
+    text = "!@#$%^&*()_+"
+    mrex_obj = mrex.exactly(text).repeat_zero_or_more()
+    assert mrex_obj.find(text).group() == text
+    assert mrex_obj.find(text * 2).group() == text * 2
+
+
+def test_repeat_one_or_more_should_return_none_on_fail():
+    assert mrex.DIGIT.repeat_one_or_more().find("abcde") is None
+
+
+# optional()
+
+
+def test_optional_should_return_match_on_success():
+    text = "!@#$%^&*()_+"
+    mrex_obj = mrex.exactly(text).optional()
+    assert mrex_obj.find(text).group() == text
+    assert mrex_obj.find(text * 2).group() == text
+    assert mrex_obj.find("").group() == ""
+
+
+# find_all()
+
+
+def test_find_all_should_return_multiple_matches():
+    assert mrex.DIGITS.find_all("1 23 456") == ["1", "23", "456"]
+
+
+# group_as()
+
+
+def test_group_as_should_return_grouped_match():
+    name = "group_name"
+    text = "!@#$%^&*()_+"
+    assert mrex.ALL.group_as(name).find(text).group(name) == text
+
+
+# or_()
+
+
+def test_or_should_return_match_on_either_input():
+    mrex_obj = mrex.DIGITS.or_(mrex.CHARS)
+    assert mrex_obj.find("123").group() == "123"
+    assert mrex_obj.find("abc").group() == "abc"
+
+
+def test_or_should_return_none_on_match_fail():
+    assert mrex.DIGITS.or_(mrex.CHARS).find("!!!") is None
+
+
+# and_()
+
+
+def test_and_should_return_match_on_success():
+    assert mrex.DIGITS.and_(mrex.NON_DIGITS).find("123abc").group() == "123abc"
+
+
+def test_and_should_return_none_on_match_fail():
+    assert mrex.DIGITS.and_(mrex.NON_DIGITS).find("abc123") is None
